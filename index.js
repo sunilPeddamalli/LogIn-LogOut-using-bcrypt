4cf6c04f6acbb2ba6f5642eae9e3c354df50bf6d
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-07-31 19:20:28 -0700

    created user modal and register post route which hash the password from request body and saves the hashed password to DB

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./modals/user')
+
+mongoose.connect('mongodb://localhost/bcryptDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to MongoDB');
+    }).catch((err) => {
+        console.log('MongoDB Error!');
+        console.log(err);
+    });
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.get('/', (req, res) => {
+    res.send('Home Page!');
+});
+
+
 app.get('/register', (req, res) => {
     res.render('register');
 });
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12)
+    const user = new User({
+        username,
+        password: hash
+    })
+    user.save();
+    res.redirect('/')
+})
 
 app.get('/secret', (req, res) => {
     res.send('You need to login to know the secret');
